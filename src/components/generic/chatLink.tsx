0753b40a7390f3dc54d0ef05fc12ccc1eb1e193d
openvpnhub <40650426+openvpnhub@users.noreply.github.com>
2020-04-15 15:53:36 +0530

    Update chatLink.tsx

--- src/components/generic/chatLink.tsx
+++ src/components/generic/chatLink.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+/* import React from 'react';
 import {connect} from 'react-redux';
 import getScript from 'scriptjs';
 
@@ -47,10 +47,10 @@ class ChatLink extends React.Component<Props, State> {
 		const { hasIcon = false, tawkToStatus } = this.props;
 
 		return (
-		<!--	<a className="chat-link" onClick={() => { Tawk_API.toggle() }}>
+	 	<a className="chat-link" onClick={() => { Tawk_API.toggle() }}>
                 {hasIcon && <FontAwesomeIcon icon={faComment} className="mr-2" /> }
 				{`Live Chat${tawkToStatus ? ` (${tawkToStatus})` : '' }`}
-			</a> -->
+			</a>
 		);
 	}
 }
@@ -66,3 +66,4 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(ChatLink);
+*/
